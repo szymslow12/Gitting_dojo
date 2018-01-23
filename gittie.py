58bbfdd81b5d33e2059cb60a2e6254625ecfa8df
Paweł Popowicz <p.p.popowicz@gmail.com>
2018-01-23 10:47:06 +0100

    Jokes added

--- gittie.py
+++ gittie.py
@@ -1,1 +1,8 @@
 print ('Hello, I'm Gittie!')
+def joke():
+    answer = (input = 'Hello, wanna hear a joke? Press  -j - key." )
+    if answer == 'j'
+        print ("How many software engineers does it take to change a light bulb? \
+        None. That's a hardware problem.")
+    else: 
+        return joke() 
