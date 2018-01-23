9703528a41795b83ee1a85f740490f59479f7761
szymslow12 <szym.slow12@gmail.com>
2018-01-23 10:58:33 +0100

    Introduce has been changed

--- gittie.py
+++ gittie.py
@@ -1,11 +1,5 @@
-print ('Hello, I'm Gittie!')
-def joke():
-    answer = (input = 'Hello, wanna hear a joke? Press  -j - key." )
-    if answer == 'j'
-        print ("How many software engineers does it take to change a light bulb? \
-        None. That's a hardware problem.")
-    else: 
-        return joke() 
+def introduce():
+    print ("Hello, I'm Gittie! Nice to meet you!")
 def add(a, b):
     a = 5
     b = 3
