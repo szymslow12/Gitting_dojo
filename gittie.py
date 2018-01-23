19f3a864d2d2cd032cdc9898a19e5389fad63334
szymslow12 <szym.slow12@gmail.com>
2018-01-23 11:01:45 +0100

    Works

--- gittie.py
+++ gittie.py
@@ -1,5 +1,11 @@
-def introduce():
-    print ("Hello, I'm Gittie! Nice to meet you!")
+print ('Hello, I'm Gittie!')
+def joke():
+    answer = (input = 'Hello, wanna hear a joke? Press  -j - key." )
+    if answer == 'j'
+        print ("How many software engineers does it take to change a light bulb? \
+        None. That's a hardware problem.")
+    else:
+        return joke()
 def add(a, b):
     a = 5
     b = 3
