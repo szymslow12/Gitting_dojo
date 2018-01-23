9b32fca26debb567378548970374b4445d6f331b
Paweł Popowicz <p.p.popowicz@gmail.com>
2018-01-23 11:12:08 +0100

    introduce func

--- gittie.py
+++ gittie.py
@@ -1,12 +1,30 @@
 print ('Hello, I'm Gittie!')
+
+
+def introduce():   
+    decision = input ("Help You? If wanna I tell you something press -t-. \ Wanna know \
+result of adding 5 to 3, pres -a-. Are you fed up with it, press -q-.")
+    if decision == 't':
+        joke ()
+    elif decision == 'a':
+        add ()5,3)
+    elif decision ==  'q':
+        quit
+    
+
+
 def joke():
     answer = (input = 'Hello, wanna hear a joke? Press  -j - key." )
     if answer == 'j'
         print ("How many software engineers does it take to change a light bulb? \
         None. That's a hardware problem.")
     else: 
-        return joke() 
-def add(a, b):
+        return joke()
+              
+def add(a,b):
     a = 5
     b = 3
-return str(a) + " " + str(b)
+    return str(a) + " " + str(b)
+
+
+start()
